31b5b288398d6ce1ac380c86ec8f854870e50957
TristanAntonsen <tristan.antonsen@gmail.com>
2022-06-08 16:16:48 -0500

    added mesh hashmap function

--- stl-ops/src/main.rs
+++ stl-ops/src/main.rs
@@ -1,17 +1,16 @@
 mod linalg;
 mod calcs;
 mod parsing;
-use std::collections::HashMap;
 
 fn main() {
     
-    let mesh = parsing::import(String::from("../stl/sphere.stl"));
+    let mesh = parsing::import(String::from("../stl/cube_binary.stl"));
     let tricount = mesh.triangles().len();
     let volume = calcs::calculate_volume(&mesh);
     let surface_area = calcs::calculate_surface_area(&mesh);
     let centroid = calcs::calculate_centroid(&mesh);
     
-    
+    parsing::create_mesh(mesh);
     println!("Triangle count: {}",tricount);
     println!("Volume: {}",volume);
     println!("Surface Area: {}",surface_area);
--- stl-ops/src/parsing.rs
+++ stl-ops/src/parsing.rs
@@ -1,9 +1,10 @@
 use std::io::BufReader;
 use std::fs::File;
-use std::env;
+use std::collections::HashMap;
+// use std::env;
 
 pub fn import(path: String) -> nom_stl::Mesh {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
     // let filename = &args[1];
     // let file = File::open(filename).unwrap();
     let file = File::open(path).unwrap();
@@ -11,3 +12,29 @@ pub fn import(path: String) -> nom_stl::Mesh {
     let mesh: nom_stl::Mesh = nom_stl::parse_stl(&mut root_vase).unwrap();
     mesh
 }
+
+pub fn create_mesh(nom_mesh: nom_stl::Mesh) -> HashMap<i32, [[f32; 3]; 3]> {
+
+    let nom_tris = nom_mesh.triangles();
+    let mut tris = HashMap::new();
+    let mut i = 0;
+
+    for triangle in nom_tris {
+        let vertices = triangle.vertices();
+        tris.insert(i, vertices);
+        i = i + 1;
+    }
+
+
+    // println!("{:?}",vertex_ids.get(&1))
+    for (key, value) in &tris {
+        println!("{}: {:?}",key, value);
+    }
+
+    tris    
+
+}
+
+// Structure for mesh:
+// vertex id hash map: {id : [x, y, z]}
+// edge hash map: {id: [vertex id1, vertex id2]}
