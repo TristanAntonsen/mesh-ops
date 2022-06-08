105008fcd503d4f545d8f0d910407485cd219364
TristanAntonsen <tristan.antonsen@gmail.com>
2022-06-08 14:07:04 -0500

    removed old loop

--- stl-ops/src/main.rs
+++ stl-ops/src/main.rs
@@ -10,19 +10,8 @@ fn main() {
     let volume = calcs::calculate_volume(&mesh);
     let surface_area = calcs::calculate_surface_area(&mesh);
     let centroid = calcs::calculate_centroid(&mesh);
-    // println!("{:?}",mesh.triangles());
-    // let mut mesh_map = HashMap::new();
-
-    let mut i = 0;
-    while i < tricount {
-        let triangle = mesh.triangles()[i];
-        let normal = triangle.normal();
-        let vertices = triangle.vertices();
-        i = i + 1;
-    }
     
     
-    // print_type_of(&mesh_map);
     println!("Triangle count: {}",tricount);
     println!("Volume: {}",volume);
     println!("Surface Area: {}",surface_area);
