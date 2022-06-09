9a5b1a0a8f5feac460fed181149118355d368b3d
TristanAntonsen <tristan.antonsen@gmail.com>
2022-06-09 18:02:49 -0500

    converted all to Mesh struct

--- stl-ops/src/calcs.rs
+++ stl-ops/src/calcs.rs
@@ -1,13 +1,14 @@
 use crate::linalg;
 use crate::parsing;
 
-pub fn calculate_surface_area(mesh: &nom_stl::Mesh) -> f32 {
-    let triangles = mesh.triangles();
+pub fn calculate_surface_area(mesh: &parsing::Mesh) -> f32 {
+    let triangles = &mesh.triangles;
     let mut area = 0.0;
-
+    
     for triangle in triangles {
-
-        let tri_area = calculate_triangle_area(triangle);
+        let triangle = *triangle as usize;
+        let vertices = mesh.vertices[triangle];
+        let tri_area = calculate_triangle_area(vertices);
         area = area + tri_area;
     }
     
@@ -15,11 +16,11 @@ pub fn calculate_surface_area(mesh: &nom_stl::Mesh) -> f32 {
 }
 
 
-pub fn calculate_triangle_area(triangle: &nom_stl::Triangle) -> f32 {
+pub fn calculate_triangle_area(triangle: [[f32; 3]; 3]) -> f32 {
 
-    let a = triangle.vertices()[0];
-    let b = triangle.vertices()[1];
-    let c = triangle.vertices()[2];
+    let a = triangle[0];
+    let b = triangle[1];
+    let c = triangle[2];
 
     let ab = [b[0]-a[0], b[1]-a[1], b[2]-a[2]];
     let ac = [c[0]-a[0], c[1]-a[1], c[2]-a[2]];
@@ -34,8 +35,8 @@ pub fn calculate_triangle_area(triangle: &nom_stl::Triangle) -> f32 {
 
 // fn calculate_triangle_area(triangle)
 
-pub fn calculate_centroid(mesh: &nom_stl::Mesh) -> Vec<f32> {
-    let triangles = mesh.triangles();
+pub fn calculate_centroid(mesh: parsing::Mesh) -> Vec<f32> {
+    let triangles = mesh.triangles;
     let tri_count : f32 = triangles.len() as f32;
     let mut centroid : Vec<f32> = Vec::new();
     let mut cx : f32 = 0.0;
@@ -43,11 +44,13 @@ pub fn calculate_centroid(mesh: &nom_stl::Mesh) -> Vec<f32> {
     let mut cz : f32 = 0.0;
     
     for triangle in triangles {
-        
+        let triangle = triangle as usize;
 
-        let v1 = triangle.vertices()[0];
-        let v2 = triangle.vertices()[1];
-        let v3 = triangle.vertices()[2];
+        let vertices = mesh.vertices[triangle];
+    
+        let v1 = vertices[0];
+        let v2 = vertices[1];
+        let v3 = vertices[2];
 
         let x_avg = (v1[0] + v2[0] + v3[0]) / 3.0;
         let y_avg = (v1[1] + v2[1] + v3[1]) / 3.0;
@@ -70,14 +73,14 @@ pub fn calculate_centroid(mesh: &nom_stl::Mesh) -> Vec<f32> {
 }
 
 
-pub fn calculate_volume(mesh: parsing::Mesh) -> f32 {
-    let triangles = mesh.triangles;
-    let vertices = mesh.vertices;
+pub fn calculate_volume(mesh: &parsing::Mesh) -> f32 {
+    let triangles = &mesh.triangles;
+    let vertices = &mesh.vertices;
     let mut volume = 0.0;
     
     
     for triangle in triangles {
-        let triangle = triangle as usize;
+        let triangle = *triangle as usize;
         let tri_verts = vertices[triangle]; 
         let v1 = tri_verts[0];
         let v2 = tri_verts[1];
--- stl-ops/src/main.rs
+++ stl-ops/src/main.rs
@@ -6,16 +6,15 @@ fn main() {
     
     let mesh = parsing::import(String::from("../stl/flourite.stl"));
     let tricount = mesh.triangles.len();
-    let volume = calcs::calculate_volume(mesh);
-    // let surface_area = calcs::calculate_surface_area(&mesh);
-    // let centroid = calcs::calculate_centroid(&mesh);
+    let volume = calcs::calculate_volume(&mesh);
+    let surface_area = calcs::calculate_surface_area(&mesh);
+    let centroid = calcs::calculate_centroid(mesh);
     
     println!("{:?}",tricount);
-    // println!("{:?}",test_mesh.vertices);
-    // println!("Triangle count: {}",tricount);
+    println!("Triangle count: {}",tricount);
     println!("Volume: {:.0} mm^3",volume);
-    // println!("Surface Area: {:.0} mm^2",surface_area);
-    // println!("Centroid: ({:.3},{:.3},{:.3})",centroid[0],centroid[1],centroid[2]);
+    println!("Surface Area: {:.0} mm^2",surface_area);
+    println!("Centroid: ({:.3},{:.3},{:.3})",centroid[0],centroid[1],centroid[2]);
 
 }
 
